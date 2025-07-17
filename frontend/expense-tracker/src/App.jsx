441e5cb2f924e72dbca246c93e390eada26ab1b7
A1n2F <alissonfferreira22@gmail.com>
2025-07-17 12:27:42 -0300

    Front: Definindo App Routes

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,9 +1,34 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
+import { Login } from "./pages/Auth/login";
+import { SignUp } from "./pages/Auth/SignUp";
+import { Home } from "./pages/Dashboard/Home";
+import { Income } from "./pages/Dashboard/Income";
+import { Expense } from "./pages/Dashboard/Expense";
+
 export function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signUp" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
     </div>
+  )
+}
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
   )
 }
