8a3a08c95f6334c7bb8998498fbb6a90e5f46560
A1n2F <alissonfferreira22@gmail.com>
2025-07-22 15:35:21 -0300

    Backend: Creating Authentication API-Profile Image Upload

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,23 @@
 const express = require("express")
 const { protect } = require("../middleware/authMiddleware")
+const upload = require("../middleware/uploadMiddleware")
 
 const { registerUser, loginUser, getUserInfo } = require("../controllers/authController")
 
 const router = express.Router()
 
 router.post("/register", registerUser)
-
 router.post("/login", loginUser)
-
 router.get("/getUser", protect, getUserInfo)
+
+router.post("/upload-image", upload.single("image"), (request, response) => {
+    if(!request.file) {
+        return response.status(400).json({ message: "No file uploaded" })
+    }
+
+    const imageUrl = `${request.protocol}://${request.get("host")}/uploads/${request.file.filename}`
+
+    response.status(200).json({ imageUrl })
+})
 
 module.exports = router
