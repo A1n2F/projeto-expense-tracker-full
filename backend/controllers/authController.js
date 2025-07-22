3a98d5ff15c26b27331139cb886a02af4273c8d5
A1n2F <alissonfferreira22@gmail.com>
2025-07-22 15:01:14 -0300

    Backend: Creating Authentication API-Register User

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,36 @@ function generateToken(id) {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h"})
 }
 
-exports.registerUser = async (request, response) => {}
+exports.registerUser = async (request, response) => {
+    const { fullName, email, password, profileImageUrl } = request.body
+
+    if(!fullName || !email || !password) {
+        return response.status(500).json({ message: "All fields are required" })
+    }
+
+    try {
+        const existingUser = await User.findOne({ email })
+
+        if(existingUser) {
+            return response.status(400).json({ message: "Email already in use" })
+        }
+
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl
+        })
+
+        response.status(201).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id)
+        })
+    } catch (error) {
+        response.status(500).json({ message: "Error registering user", error: error.message })
+    }
+}
 
 exports.loginUser = async (request, response) => {}
 
