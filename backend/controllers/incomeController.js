c6d8006b380fb59f4e9067cc90efa3b468d9f28c
A1n2F <alissonfferreira22@gmail.com>
2025-07-22 19:58:14 -0300

    Backend: Creating Income API

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,10 @@
+const User = require("../models/User")
+const Income = require("../models/Income")
+
+exports.addIncome = async (request, response) => {}
+
+exports.getAllIncome = async (request, response) => {}
+
+exports.deleteIncome = async (request, response) => {}
+
+exports.downloadIncomeExcel = async (request, response) => {}
