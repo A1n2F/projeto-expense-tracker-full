521fa71e47b3a6e0a70c35c2937b1f24a1421bd7
A1n2F <alissonfferreira22@gmail.com>
2025-07-22 20:09:34 -0300

    Backend: Creating Income API-Add Income

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -1,7 +1,30 @@
 const User = require("../models/User")
 const Income = require("../models/Income")
 
-exports.addIncome = async (request, response) => {}
+exports.addIncome = async (request, response) => {
+    const userId = request.user.id
+
+    try {
+        const { icon, source, amount, date } = request.body
+
+        if(!source || !amount || !date) {
+            return response.status(400).json({ message: "All fields are required" })
+        }
+
+        const newIncome = new Income({
+            userId, 
+            icon,
+            source,
+            amount,
+            date: new Date(date)
+        })
+
+        await newIncome.save()
+        response.status(200).json(newIncome)
+    } catch (error) {
+        response.status(500).json({ message: "Server Error" })
+    }
+}
 
 exports.getAllIncome = async (request, response) => {}
 
