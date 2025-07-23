2c7a87faf2a0282f58268456c42654d707a1d784
A1n2F <alissonfferreira22@gmail.com>
2025-07-23 13:52:59 -0300

    Frontend: Integrating the SignUp API

--- frontend/expense-tracker/src/pages/Auth/SignUp.jsx
+++ frontend/expense-tracker/src/pages/Auth/SignUp.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+
 import { validateEmail } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import uploadImage from "../../utils/uploadImage";
 
 import { AuthLayout } from "../../components/layouts/AuthLayout";
 import { Input } from "../../components/Inputs/Input";  
 import { ProfilePhotoSelector } from "../../components/Inputs/ProfilePhotoSelector";
+
+import { UserContext } from "../../context/userContext";
 
 export function SignUp() {
     const [profilePic, setProfilePic] = useState(null)
@@ -13,6 +19,8 @@ export function SignUp() {
     const [password, setPassword] = useState("")
 
     const [error, setError] = useState(null)
+
+    const { updateUser } = useContext(UserContext)
 
     const navigate = useNavigate()
 
@@ -37,6 +45,29 @@ export function SignUp() {
         }
 
         setError("")
+
+        try {
+            if(profilePic) {
+                const imgUploadRes = await uploadImage(profilePic)
+                profileImageUrl = imgUploadRes.imageUrl || ""
+            }
+
+            const response = await axiosInstance.post(API_PATHS.AUTH.REGISTER, { fullName, email, password, profileImageUrl })
+
+            const { token, user } = response.data
+
+            if(token) {
+                localStorage.setItem("token", token)
+                updateUser(user)
+                navigate("/dashboard")
+            }
+        } catch (error) {
+            if(error.response && error.response.data.message) {
+                setError(error.response.data.message)
+            } else {
+                setError("Something went wrong. Please try again.")
+            }
+        }
     }
 
     return (
