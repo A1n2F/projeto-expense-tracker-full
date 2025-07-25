164f2e898d34af4adf449a8c38b5a440b174945b
A1n2F <alissonfferreira22@gmail.com>
2025-07-25 10:38:24 -0300

    Frontend: Layout-Recent Income

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -14,6 +14,7 @@ import { FinanceOverview } from "../../components/Dashboard/FinanceOverview";
 import { ExpenseTransactions } from "../../components/Dashboard/ExpenseTransactions";
 import { Last30DaysExpenses } from "../../components/Dashboard/last30DaysExpenses";
 import { RecentIncomeWithChart } from "../../components/Dashboard/RecentIncomeWithChart";
+import { RecentIncome } from "../../components/Dashboard/RecentIncome";
 
 import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
 import { IoMdCard } from "react-icons/io"
@@ -104,6 +105,11 @@ export function Home() {
                     <RecentIncomeWithChart
                         data={dashboardData?.last60DaysIncome?.transactions?.slice(0,4) || []}
                         totalIncome={dashboardData?.totalIncome || 0}
+                    />
+
+                    <RecentIncome
+                        transactions={dashboardData?.last60DaysIncome?.transactions || []}
+                        onSeeMore={() => navigate("/income")}
                     />
                 </div>
             </div>
